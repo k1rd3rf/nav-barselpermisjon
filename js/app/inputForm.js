e979aff3cebd82edd4d18bcad6fbb07bea2f5599
Fredrik Oterholt <fredrik.oterholt@embriq.no>
2015-02-08 22:31:19 +0100

    Made public variable for component name.

--- js/app/inputForm.js
+++ js/app/inputForm.js
@@ -2,67 +2,69 @@ define(['knockout', 'app/maternityLeave', 'text!view/inputForm.html'], function 
     'use strict';
 
 
-    var InputForm = function (options) {
-        var form = this,
-            defaults = {
-                birthDate: ko.observable(),
-                dueDate: ko.observable()
+    var componentName = 'input-form',
+        InputForm = function (options) {
+            var form = this,
+                defaults = {
+                    birthDate: ko.observable(),
+                    dueDate: ko.observable()
+                };
+
+            form.options = ko.utils.extend(defaults, options);
+
+            form.permSelect = {
+                options: ko.observableArray([
+                    {
+                        weeks: 49,
+                        salary: 100,
+                        description: "Kort"
+                    },
+                    {
+                        weeks: 59,
+                        salary: 80,
+                        description: "Lang"
+                    }
+                ]),
+                render: function (item) {
+                    return item.description + " (" + item.weeks + " uker)";
+                },
+                selected: ko.observable()
             };
 
-        form.options = ko.utils.extend(defaults, options);
+            form.mothersAmount = ko.observable(39);
 
-        form.permSelect = {
-            options: ko.observableArray([
-                {
-                    weeks: 49,
-                    salary: 100,
-                    description: "Kort"
-                },
-                {
-                    weeks: 59,
-                    salary: 80,
-                    description: "Lang"
-                }
-            ]),
-            render: function (item) {
-                return item.description + " (" + item.weeks + " uker)";
-            },
-            selected: ko.observable()
+            form.fathersAmount = ko.computed(function () {
+                return (form.permSelect.selected() || {}).weeks - form.mothersAmount();
+            });
+
+            form.maxWeeks = ko.computed(function () {
+                return (form.permSelect.selected() || {}).weeks - 10;
+            });
+
+            form.birthDate = form.options.birthDate;
+            form.birthDate.extend({
+                date: true
+            });
+
+            form.dueDate = form.options.dueDate;
+            form.dueDate.extend({
+                date: true
+            });
+
+            form.maternityLeave = {
+                mother: new MaternityLeave({
+                    title: "Mor",
+                    leaveWeeks: form.mothersAmount,
+                    maxWeeks: form.maxWeeks
+                }),
+                father: new MaternityLeave({
+                    title: "Far",
+                    leaveWeeks: form.fathersAmount,
+                    maxWeeks: form.maxWeeks
+                })
+            };
+            form.name = componentName;
         };
-
-        form.mothersAmount = ko.observable(39);
-
-        form.fathersAmount = ko.computed(function () {
-            return (form.permSelect.selected() || {}).weeks - form.mothersAmount();
-        });
-
-        form.maxWeeks = ko.computed(function () {
-            return (form.permSelect.selected() || {}).weeks - 10;
-        });
-
-        form.birthDate = form.options.birthDate;
-        form.birthDate.extend({
-            date: true
-        });
-
-        form.dueDate = form.options.dueDate;
-        form.dueDate.extend({
-            date: true
-        });
-
-        form.maternityLeave = {
-            mother: new MaternityLeave({
-                title: "Mor",
-                leaveWeeks: form.mothersAmount,
-                maxWeeks: form.maxWeeks
-            }),
-            father: new MaternityLeave({
-                title: "Far",
-                leaveWeeks: form.fathersAmount,
-                maxWeeks: form.maxWeeks
-            })
-        };
-    }, componentName = 'input-form';
 
     if (!ko.components.isRegistered(componentName)) {
         ko.components.register(componentName, {template: view});
--- js/app/maternityLeave.js
+++ js/app/maternityLeave.js
@@ -2,33 +2,34 @@ define(['knockout', 'text!view/maternityLeave.html'], function (ko, view) {
     'use strict';
 
 
-    var MaternityLeave = function (options) {
-        var leave = this,
-            defaults = {
-                vacationDays: ko.observable(),
-                leaveWithoutPay: ko.observable(),
-                leaveWeeks: ko.observable(10),
-                title: ko.observable(),
-                maxWeeks: ko.observable(39)
-            };
+    var componentName = 'maternity-leave-parent',
+        MaternityLeave = function (options) {
+            var leave = this,
+                defaults = {
+                    vacationDays: ko.observable(),
+                    leaveWithoutPay: ko.observable(),
+                    leaveWeeks: ko.observable(10),
+                    title: ko.observable(),
+                    maxWeeks: ko.observable(39)
+                };
 
-        leave.options = ko.utils.extend(defaults, options);
+            leave.options = ko.utils.extend(defaults, options);
 
-        leave.vacationDays = leave.options.vacationDays;
-        leave.leaveWeeks = leave.options.leaveWeeks;
-        leave.leaveWithoutPay = leave.options.leaveWithoutPay;
-        leave.title = leave.options.title;
+            leave.vacationDays = leave.options.vacationDays;
+            leave.leaveWeeks = leave.options.leaveWeeks;
+            leave.leaveWithoutPay = leave.options.leaveWithoutPay;
+            leave.title = leave.options.title;
 
-        leave.leaveWeeks.extend({
-            min: 10,
-            max: leave.options.maxWeeks
-        });
+            leave.leaveWeeks.extend({
+                min: 10,
+                max: leave.options.maxWeeks
+            });
 
-        leave.isPermWeeksComputed = ko.computed(function () {
-            return ko.isComputed(leave.leaveWeeks);
-        });
-
-    }, componentName = 'maternity-leave-parent';
+            leave.isPermWeeksComputed = ko.computed(function () {
+                return ko.isComputed(leave.leaveWeeks);
+            });
+            leave.name = componentName;
+        };
 
     if (!ko.components.isRegistered(componentName)) {
         ko.components.register(componentName, {template: view});
