549d1e7e1be303af7ac38e7d6d49f7a95750fd87
Fredrik Oterholt <fredrik.oterholt@embriq.no>
2015-02-11 22:21:17 +0100

    Added vis dependency. Initial vis-setup.

--- js/app/maternitySummary.js
+++ js/app/maternitySummary.js
@@ -1,6 +1,5 @@
-define(['knockout', 'text!view/maternitySummary.html'], function (ko, view) {
+define(['knockout', 'vis', 'jquery', 'text!view/maternitySummary.html', 'domReady!'], function (ko, vis, $, view) {
     'use strict';
-
 
     var componentName = 'maternity-leave-summary',
         MaternitySummary = function (options) {
@@ -9,6 +8,20 @@ define(['knockout', 'text!view/maternitySummary.html'], function (ko, view) {
 
             leave.options = ko.utils.extend(defaults, options);
             leave.name = componentName;
+
+
+            leave.groups = new vis.DataSet([
+                {id: 'mother', content: 'mother'},
+                {id: 'father', content: 'father'}
+            ]);
+
+            leave.items = new vis.DataSet({
+                type: {start: 'ISODate', end: 'ISODate'}
+            });
+
+            leave.options = {};
+            //leave.timeline = new vis.Timeline($('.maternitySummaryVis'), leave.items, leave.groups, leave.options);
+
         };
 
     if (!ko.components.isRegistered(componentName)) {
--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ requirejs.config({
     "baseUrl": "js",
     "paths": {
         "text": "../bower_components/requirejs-text/text",
+        "domReady": "../bower_components/requirejs-domready/domReady",
         "moment": "../bower_components/moment/min/moment.min",
         "underscore": "../bower_components/underscore/underscore-min",
         "jquery": "../bower_components/jquery/dist/jquery.min",
@@ -9,7 +10,8 @@ requirejs.config({
         "knockout": "../bower_components/knockout/dist/knockout",
         "knockout.validation": "../bower_components/knockout-validation/dist/knockout.validation.min",
         "kvLocale": "../bower_components/knockout-validation/localization/",
-        "i18next": "../bower_components/i18next/i18next.amd.min"
+        "i18next": "../bower_components/i18next/i18next.amd.min",
+        "vis": "../bower_components/vis/dist/vis.min"
     },
     shim: {
         "bootstrap": {"deps": ['jquery']}
